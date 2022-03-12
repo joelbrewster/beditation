32a92ea3c7d06caac3dc5388450117dfb27189d3
Joel Brewster <hi@joelbrewster.com>
2022-03-12 16:25:20 +1100

    Revert "rfinni"
    This reverts commit 1badf750f4a82fa714fe7dc1f755ab4e227c007b.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,102 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import AudioPlayer from "./AudioPlayer";
-import tracks from "./tracks";
+import "./index.css";
+
+const rain = "./audio/rain.mp3";
+const wind = "./audio/wind.mp3";
+const noise = "./audio/noise.mp3";
+const ocean = "./audio/ocean.mp3";
+
+const TRACKS = [
+    { id: 1, title: "rain" },
+    { id: 2, title: "wind" },
+    { id: 3, title: "ocean" },
+    { id: 4, title: "noise" },
+];
+
+class App extends React.Component {
+    state = {
+	selectedTrack: null,
+	player: "stopped",
+    };
+
+    componentDidUpdate(prevProps, prevState) {
+	if (this.state.selectedTrack !== prevState.selectedTrack) {
+	    let track;
+	    switch (this.state.selectedTrack) {
+            case "rain":
+		track = rain;
+		break;
+            case "wind":
+		track = wind;
+		break;
+	    case "ocean":
+		track = ocean;
+		break;
+	    case "noise":
+		track = noise;
+		break;
+            default:
+		break;
+	    }
+	    if (track) {
+		this.player.src = track;
+		this.player.play();
+		this.setState({ player: "playing", duration: this.player.duration });
+	    }
+	}
+
+	if (this.state.player !== prevState.player) {
+	    if (this.state.player === "stopped") {
+		this.player.pause();
+		this.player.currentTime = 0;
+		this.setState({ selectedTrack: null });
+	    } else if (
+		this.state.player === "playing" &&
+		    prevState.player === "paused"
+	    ) {
+		this.player.play();
+	    }
+	}
+    }
+
+    render() {
+	const list = TRACKS.map((item) => {
+	    return (
+		<div
+		    className="col tracks"
+		    key={item.id}
+		    onClick={() => this.setState({ selectedTrack: item.title })}
+		    style={{
+			fontWeight: item.title === this.state.selectedTrack && "bold",
+		    }}
+		>
+		    {item.title}
+		</div>
+	    );
+	});
+
+	return (
+	    <>
+		<div className="player">
+		    <div className="tracklist col">{list}</div>
+
+		    <div className={this.state.player !== 'stopped' ? 'buttons active' : 'buttons '}>
+			<button onClick={() => this.setState({ player: "stopped" })}>
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 1000">
+				    <path d="M500 10C229.4 10 10 229.4 10 500s219.4 490 490 490 490-219.4 490-490S770.6 10 500 10zm0 918.7C263.6 928.7 71.3 736.4 71.3 500 71.3 263.6 263.6 71.3 500 71.3c236.4 0 428.7 192.3 428.7 428.7 0 236.4-192.3 428.7-428.7 428.7z" />
+				    <path d="M622.5 316.2h-245c-33.8 0-61.2 27.4-61.2 61.2v245c0 33.8 27.4 61.3 61.2 61.3h245c33.8 0 61.3-27.4 61.3-61.3v-245c-.1-33.7-27.5-61.2-61.3-61.2z" />
+				</svg>
+			    </button>
+			</div>
+
+			</div>
+			<audio ref={(ref) => (this.player = ref)} />
+	    </>
+	);
+    }
+}
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <React.StrictMode>
-    <AudioPlayer tracks={tracks} />
-  </React.StrictMode>,
-  rootElement
-);
+ReactDOM.render(<App />, rootElement);
+
